2945c57a7013ed67459ef23f45a2fd1d7e7714fe
KRISHNAPRASADEK <kudu380013@gmail.com>
2023-01-28 20:53:15 +0530

    contactApp-mean v1.3 api connected

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,14 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 import { MyContact } from 'src/models/myContact';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  baseUrl: string = environment.API_URL;
+  baseUrl: string = 'https://ill-puce-crane-coat.cyclic.app';
   // http://localhost:3000/contacts
 
   constructor(private http: HttpClient) {}
