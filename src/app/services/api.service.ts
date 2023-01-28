78d8232c28da8e048f1a34a858d6dee9551afb5a
KRISHNAPRASADEK <kudu380013@gmail.com>
2023-01-28 16:47:15 +0530

    set dotenv hide api url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,53 +1,52 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { MyContact } from 'src/models/myContact';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  baseUrl: string = 'https://contactapp-api.onrender.com/contacts';
+  baseUrl: string = environment.API_URL;
   // http://localhost:3000/contacts
 
   constructor(private http: HttpClient) {}
 
   //function for get all contacts
   getAllContacts(): Observable<MyContact> {
-    return this.http.get(this.baseUrl);
+    return this.http.get(this.baseUrl + '/contacts');
   }
 
   //function for view a particular contact
   viewContact(contactId: string) {
-    return this.http.get(`${this.baseUrl}/${contactId}`);
+    return this.http.get(`${this.baseUrl}/contacts/${contactId}`);
   }
 
   //function for getting particular group name
   getGroupName(groupId: string) {
-    return this.http.get(
-      'https://contactapp-api.onrender.com/groups/' + groupId
-    );
+    return this.http.get(this.baseUrl + '/groups/' + groupId);
   }
 
   // http://localhost:3000/groups
 
-  //function for fetch all groups from https://contact-api-e6s0.onrender.com/groups
+  //function for fetch all groups from
   getAllGroups() {
-    return this.http.get('https://contact-api-e6s0.onrender.com/groups');
+    return this.http.get(this.baseUrl + '/groups');
   }
 
   //function for adding new contacts to server
   addContact(contactBody: any) {
-    return this.http.post(this.baseUrl, contactBody);
+    return this.http.post(this.baseUrl + '/contacts', contactBody);
   }
 
   //function for deleting a contact
   deleteContact(contactId: any) {
-    return this.http.delete(`${this.baseUrl}/${contactId}`);
+    return this.http.delete(`${this.baseUrl}/contacts/${contactId}`);
   }
 
   // update contact detailes according to the user select
   updateContact(contactId: any, contactBody: any) {
-    return this.http.put(`${this.baseUrl}/${contactId}`, contactBody);
+    return this.http.put(`${this.baseUrl}/contacts/${contactId}`, contactBody);
   }
 }
